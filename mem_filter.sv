ed78da56a9053cf6f2ebfa8e6587c0bb663b3557
panliu0924 <144409910+panliu0924@users.noreply.github.com>
2024-04-05 12:03:15 -0700

    Add files via upload

--- mem_filter.sv
+++ mem_filter.sv
@@ -44,25 +44,48 @@ always @(posedge DFF_clk) begin
 end
 endmodule
 
-module FF_data_2X(
-input [7:0] Ldata1,
-input [7:0] Ldata2,
+module FF_data_4X(
+input [WIDTH_data-1:0] Ldata,
+input [WIDTH_addr-1:0] Laddr,
+input [WIDTH_dest-1:0] Ldest,
+input [WIDTH_datatype-1:0] Ldatatype,
 input DFF_clk,
-output reg [7:0] Rdata1,
-output reg [7:0] Rdata2);
+output reg [WIDTH_data-1:0] Rdata,
+output reg [WIDTH_addr-1:0] Raddr,
+output reg [WIDTH_dest-1:0] Rdest,
+output reg [WIDTH_datatype-1:0] Rdatatype,);
+parameter WIDTH_data = 8;
+parameter WIDTH_addr = 12;
+parameter WIDTH_dest = 4;
+parameter WIDTH_datatype = 2;
+
 always @(posedge DFF_clk) begin
-Rdata1 <= #2 Ldata1;
-Rdata2 <= #2 Ldata2;
+Rdata <= #2 Ldata;
+Raddr <= #2 Laddr;
+Rdest <= #2 Ldest;
+Rdatatype <= #2 Ldatatype;
 end
 endmodule
 
-module click_controller(input Lreq, input Rack, input [7:0] Ldata, output Lack, output reg Rreq, output [7:0] Rdata);
+module click_controller_mem_filter(input Lreq, input Rack, output Lack, output reg Rreq, 
+input [WIDTH_data-1:0] Ldata, output [WIDTH_data-1:0] Rdata
+input [WIDTH_addr-1:0] Laddr, output [WIDTH_addr-1:0] Raddr)
+input [WIDTH_dest-1:0] Ldest, output [WIDTH_dest-1:0] Rdest)
+input [WIDTH_datatype-1:0] Ldatatype, input [WIDTH_datatype-1:0] Rdatatype);
+parameter WIDTH_data = 8;
+parameter WIDTH_addr = 12;
+parameter WIDTH_dest = 4;
+parameter WIDTH_datatype = 2;
 logic out1, out2, DFF_clk, Rreq_in, Rreq_out;
 AND_Gates AND0(.Lreq(Lreq), .Rack(Rack), .Rreq(Rreq), .out1(out1), .out2(out2));
 OR OR0(.in1(out1), .in2(out2), .DFF_clk(DFF_clk));
 Invert INVERT(.Rreq(Rreq), .Rreq_inverted(Rreq_in));
 FF_req FF0(.Rreq_in(Rreq_in),.Rreq_out(Rreq_out), .DFF_clk(DFF_clk));
-FF_data FF1(.Ldata(Ldata), .Rdata(Rdata), .DFF_clk(DFF_clk));
+FF_data_4X FF_4X(.Ldata(Ldata), .Rdata(Rdata), 
+.Laddr(Laddr), .Raddr(Raddr), 
+.Ldest(Ldest), .Rdest(Rdest),
+.Ldatatype(Ldatatype), Rdatatype(Rdatatype),
+.DFF_clk(DFF_clk));
 initial begin
     DFF_clk = 0;
     Rreq = 0;
@@ -80,6 +103,8 @@ parameter FL = 12;
 parameter BL = 4;
 parameter WIDTH_data = 8;
 parameter WIDTH_addr = 12;
+parameter WIDTH_dest = 4;
+parameter WIDTH_datatype = 2;
 parameter WIDTH_out_data = 13;
 parameter DEPTH_F= 5;
 parameter DEPTH_I =25;
@@ -88,40 +113,51 @@ parameter DEPTH_R =21;
 //Data Signals
 logic [WIDTH_addr-1:0] f_addr;
 logic [WIDTH_data-1:0] f_data;
+logic [WIDTH_addr-1:0] Raddr;
+logic [WIDTH_data-1:0] Rdata;
 logic [WIDTH_data-1:0] mem_filter[DEPTH_F*DEPTH_F-1:0];
-logic [1:0] Datatype; //Sent to Packetize
-logic [1:0] Dest; //Sent to Packetize
+logic [WIDTH_datatype-1:0] Ldatatype;
+logic [WIDTH_datatype-1:0] Rdatatype;
+logic [WIDTH_dest-1:0] Ldest; 
+logic [WIDTH_dest-1:0] Rdest; 
 //-------------------------------------------------------------//
 //Click Controller Signals
 logic Lreq, Rack, Rreq, Lack;
 logic Lpacket, Rpacket;
-click_controller CCT0(.Lreq(Lreq), .Rack(Rack), .Ldata(Lpacket), .Rreq(Rreq), .Lack(Lack), .Rdata(Rpacket));
+click_controller_mem_filter CCT_M_filter(.Lreq(Lreq), .Rack(Rack), .Rreq(Rreq), .Lack(Lack), 
+.Ldata(f_data), .Rdata(Rdata), 
+.Laddr(f_addr), .Raddr(Raddr), 
+.Ldest(Ldest), .Rdest(Rdest),
+.Ldatatype(Ldatatype), Rdatatype(Rdatatype),);
+packetize_mem_filter PCK_filter(interface router, .addr(), .data(), .dest(), .datatype());
 //-------------------------------------------------------------//
 initial begin
-
-
+f_data = 0;
+f_addr = 0;
+Ldest = 0;
+Ldatatype = 0;
 end
 
 always @(*) begin
-//send data
 #BL;
 Lreq = 1;
 noc_snn.Receive(f_addr);
 noc_snn.Receive(f_data);
-mem_filter[f_addr] = f_data;
-Datatype = 0;
-Dest = 1;
+L_addr = f_addr;
+L_data = f_data;
+Ldest = 4'b1111;
+Ldatatype = 0; //Filter
 #FL;
-
 wait(Lack == 1);
 //send data
 #BL;
 Lreq = 0;
 noc_snn.Receive(f_addr);
 noc_snn.Receive(f_data);
-Ldata_f_addr = f_addr;
-Ldata_f_data = f_data;
-mem_filter[f_addr] = f_data;
+L_addr = f_addr;
+L_data = f_data;
+Ldest = 4'b1111;
+Ldatatype = 0; //Filter
 #FL;
 wait(Lack==0);
 end
@@ -132,5 +168,16 @@ wait(Rreq == 1);
 Rack = 1;
 wait(Rreq == 0);
 end
+
+endmodule
+
+module packetize_mem_filter(output reg [63:0] packet)
+always begin
+
+#BL
+
+
+end
+
 
 endmodule
